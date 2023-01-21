9dcb31944b2711b78f6c0112d08f36b3ebb3c77a
Karinne Sagir <karinsagir@karinnes-mbp.lan>
2023-01-21 14:27:08 -0800

    added uniqid to notes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const util = require("util");
 const router = require("express").Router();
+var uniqid = require('uniqid');
 
 
 router.get("/notes", (req, res) => {
@@ -16,16 +17,16 @@ router.get("/notes", (req, res) => {
 // front end to back end
 router.post("/notes", (req, res) => {
 
-
-
   fs.readFile("db/db.json", (err, data) => {
     if(err) throw err
     notes = JSON.parse(data);
+    req.body.id = uniqid()
     notes.push(req.body);
 
   fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
     if(err) throw err
     res.json(notes);
+
   });
 
   })
